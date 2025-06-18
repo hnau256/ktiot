2c85278aa18eba803902637b77190d2e1fb56e99
Mark Zorikhin <hnau256@gmail.com>
2025-06-18 20:09:39 +0300

    coordinator maven publisher

--- ktiot/coordinator/build.gradle.kts
+++ ktiot/coordinator/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     alias(libs.plugins.kotlin.serialization)
     kotlin("jvm")
+    id("java-library")
+    id("maven-publish")
+}
+
+java {
+    val javaVersion = JavaVersion.valueOf(libs.versions.java.get())
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
 }
 
 dependencies {
@@ -14,4 +22,27 @@ dependencies {
     implementation(project(":common:logging"))
     implementation(project(":common:mqtt"))
     implementation(project(":ktiot:scheme"))
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(project(":common:logging").tasks.named("compileKotlinDesktop").get().outputs.files)
+    from(project(":common:mqtt").tasks.named("compileKotlinDesktop").get().outputs.files)
+    from(project(":ktiot:scheme").tasks.named("compileKotlinDesktop").get().outputs.files)
+}
+
+group = "com.github.hnau256"
+version = "1.0.0"
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = project.group as String
+            version = project.version as String
+        }
+    }
 }
