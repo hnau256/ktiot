0309cfce3646139d3c44af110f2c465207a9c55e
Mark Zorikhin <hnau256@gmail.com>
2026-02-02 18:36:17 +0200

    UI Fixes

--- ktiot/client/projector/src/commonMain/kotlin/hnau/ktiot/client/projector/screen/ScreenProjector.kt
+++ ktiot/client/projector/src/commonMain/kotlin/hnau/ktiot/client/projector/screen/ScreenProjector.kt
@@ -5,22 +5,26 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import arrow.core.Either
 import hnau.common.app.projector.uikit.state.LoadableContent
+import hnau.common.app.projector.uikit.state.StateContent
 import hnau.common.app.projector.uikit.state.TransitionSpec
 import hnau.common.app.projector.uikit.utils.Dimens
+import hnau.common.app.projector.utils.SlideOrientation
+import hnau.common.app.projector.utils.getTransitionSpecForSlide
 import hnau.common.app.projector.utils.plus
-import hnau.common.kotlin.Loadable
+import hnau.common.kotlin.*
 import hnau.common.kotlin.coroutines.createChild
 import hnau.common.kotlin.coroutines.flow.state.runningFoldState
-import hnau.common.kotlin.ifNull
-import hnau.common.kotlin.map
-import hnau.common.kotlin.valueOrElse
 import hnau.ktiot.client.model.screen.ScreenItemModel
 import hnau.ktiot.client.model.screen.ScreenModel
 import hnau.ktiot.client.model.utils.ChildTopic
@@ -34,6 +38,8 @@ import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.StateFlow
+import kotlin.math.sign
+import kotlin.time.Duration.Companion.seconds
 
 @Immutable
 class ScreenProjector(
@@ -171,27 +177,55 @@ class ScreenProjector(
                 modifier = Modifier.fillMaxSize(),
                 transitionSpec = TransitionSpec.crossfade(),
             ) { state ->
-                when (state) {
-                    is State.Child -> state
-                        .projector
-                        .Content(contentPadding)
-                    is State.Items -> Items(
-                        items = state.items,
-                        contentPadding = contentPadding,
-                    )
+                state.StateContent(
+                    modifier = Modifier.fillMaxSize(),
+                    label = "ItemsOrChild",
+                    contentKey = { it.zIndex },
+                    transitionSpec = getTransitionSpecForSlide(
+                        orientation = SlideOrientation.Horizontal,
+                    ) {
+                        (targetState.zIndex - initialState.zIndex).sign * 0.5f
+                    },
+                ) { stateLocal ->
+                    when (stateLocal) {
+                        is State.Child -> stateLocal
+                            .projector
+                            .Content(contentPadding)
+
+                        is State.Items -> Items(
+                            items = stateLocal.items,
+                            contentPadding = contentPadding,
+                        )
+                    }
                 }
             }
     }
+
+    private val Loadable<*>.zIndex: Int
+        get() = fold(
+            ifLoading = { 0 },
+            ifReady = { 1 }
+        )
+
+    private val State.zIndex: Int
+        get() = when (this) {
+            is State.Items -> 0
+            is State.Child -> 1
+        }
 
     @Composable
     private fun Items(
         items: ImmutableList<State.Items.Item>,
         contentPadding: PaddingValues,
     ) {
-        LazyColumn(
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(
+                minSize = 256.dp,
+            ),
             contentPadding = contentPadding + PaddingValues(Dimens.separation),
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(Dimens.separation),
+            horizontalArrangement = Arrangement.spacedBy(Dimens.separation),
         ) {
             items(
                 items = items,
